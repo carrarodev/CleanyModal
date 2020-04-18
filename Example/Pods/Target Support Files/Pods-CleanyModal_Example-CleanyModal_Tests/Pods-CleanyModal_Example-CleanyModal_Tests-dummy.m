a7c006d5009b346a9960f7bda570f9ef5c1fc26f
Daniele Carraro <carrarodev@gmail.com>
2020-04-18 17:20:56 +0200

    Remove all file

--- Example/Pods/Target Support Files/Pods-CleanyModal_Example-CleanyModal_Tests/Pods-CleanyModal_Example-CleanyModal_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CleanyModal_Example_CleanyModal_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CleanyModal_Example_CleanyModal_Tests
-@end
